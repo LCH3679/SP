de0fa1ecd062f0509e510b711f1d75a2205df235
lch <lch@User>
2022-03-20 12:57:01 +0900

    첫 번째

---
+++ sp_crud/src/main/java/org/iot/mapper/BoardMapper.java
@@ -0,0 +1,27 @@
+package org.iot.mapper;
+
+import java.util.List;
+
+import org.iot.domain.BoardVO;
+import org.iot.domain.Criteria;
+
+
+
+public interface BoardMapper {
+
+	public List<BoardVO> getList();
+
+	public List<BoardVO> getListWithPaging(Criteria cri);
+
+	public void insert(BoardVO board);
+
+	public Integer insertSelectKey(BoardVO board);
+
+	public BoardVO read(Long bno);
+
+	public int delete(Long bno);
+
+	public int update(BoardVO board);
+
+	public int getTotalCount(Criteria cri);
+}
